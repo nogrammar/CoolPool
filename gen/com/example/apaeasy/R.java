ee925a3d15f896584b9f9535b9234f1f5251cdd5
dnice <dnice@rapture2.(none)>
2012-07-24 01:37:14 -0500

    modified:   bin/classes/com/example/apaeasy/MainActivity$2.class 	modified:   bin/classes/com/example/apaeasy/MainActivity.class 	modified:   bin/classes/com/example/apaeasy/R$id.class 	modified:   bin/classes/com/example/apaeasy/R$layout.class 	modified:   bin/classes/com/example/apaeasy/R$menu.class 	modified:   bin/classes/com/example/apaeasy/R$string.class 	modified:   bin/classes/com/example/apaeasy/R$style.class 	new file:   bin/jarlist.cache 	modified:   gen/com/example/apaeasy/R.java 	modified:   res/layout/activity_main.xml

--- gen/com/example/apaeasy/R.java
+++ gen/com/example/apaeasy/R.java
@@ -20,15 +20,17 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int addPlayer=0x7f080001;
-        public static final int menu_settings=0x7f080008;
-        public static final int spinner1=0x7f080002;
+        public static final int addPlayer=0x7f080002;
+        public static final int editText1=0x7f080005;
+        public static final int menu_settings=0x7f08000a;
+        public static final int spinner1=0x7f080001;
         public static final int tableRow1=0x7f080000;
         public static final int tableRow2=0x7f080003;
-        public static final int tableRow3=0x7f080004;
-        public static final int tableRow4=0x7f080005;
-        public static final int tableRow5=0x7f080006;
-        public static final int tableRow6=0x7f080007;
+        public static final int tableRow3=0x7f080006;
+        public static final int tableRow4=0x7f080007;
+        public static final int tableRow5=0x7f080008;
+        public static final int tableRow6=0x7f080009;
+        public static final int textView1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
